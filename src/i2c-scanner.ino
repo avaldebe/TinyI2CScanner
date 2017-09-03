19fe51b0d40477e7e43ef83f5d863faac7597c17
Alvaro Valdebenito <alvarov@met.no>
2017-09-03 22:55:46 +0200

    OLED on BBang I2C, scan over HW I2C

--- src/i2c-scanner.ino
+++ src/i2c-scanner.ino
@@ -1,46 +1,55 @@
+// 128x64 OLED module, bitbanged I2C
+#define OLED_MAX_X (128)
+#define OLED_MAX_Y ( 64)
+
+// OLED library, bitbanged I2C
 #include <font6x8.h>
 #include <font8x16.h>
 #include <ssd1306xled.h>
 #include <ssd1306xled8x16.h>
 
+// Scan haedware I2C bus
+#include <TinyWireM.h>
+
+// Background image from tiny44 project
 #include "img.h"
 
-// OLED 128x64 pixels
-#define OLED_MAX_X (128)
-#define OLED_MAX_Y ( 64)
-
-// SSD1306Device SSD1306;
-
 void setup() {
+  // init hardware I2C buss
+  TinyWireM.begin();
+
+  // blink
+  pinMode(LED_PIN, OUTPUT);
+
+  // init OLED, bitbanged I2C bus
   SSD1306.ssd1306_init();
 
-  wellcome("I2C Scanner","7bit addresses");
-  delay(1500);
+  // welcome message
+  const byte x_mid = OLED_MAX_X/2;
+  const byte y_mid = OLED_MAX_Y/(8*2);
 
-  SSD1306.ssd1306_draw_bmp(0, 0, 128, 8, frame);
+  SSD1306.ssd1306_fillscreen(0x00);
+  SSD1306.ssd1306_char_f8x16(x_mid-12*(8/2), y_mid-1,"I2C Scanner");
+  SSD1306.ssd1306_setpos(x_mid-14*(6/2), y_mid+1);
+  SSD1306.ssd1306_string_font6x8((char*)"7bit addresses");
 }
 
 void loop() {
+  delay(2000);
 
-}
+  // background image
+  SSD1306.ssd1306_draw_bmp(0, 0, 128, 8, frame);
 
-void wellcome(const char *title, const char *subtitle) {
-  const byte x_mid = OLED_MAX_X/2;
-  const byte y_mid = OLED_MAX_Y/16;
+  digitalWrite(LED_PIN, HIGH); // start scan
 
-  SSD1306.ssd1306_fillscreen(0x00);
-
-  byte mlen = 0;
-  while(title[mlen]) mlen++;
-  if(mlen>0){
-    SSD1306.ssd1306_setpos(x_mid-mlen*3, y_mid-1);
-    SSD1306.ssd1306_string_font6x8(title);
+  uint8_t addr, error;
+  for(addr=8; addr<120; addr++){
+    TinyWireM.beginTransmission(addr);
+    error = TinyWireM.endTransmission(1);
+    if(error == 0x00) {
+      SSD1306.ssd1306_char_f8x16(0,0,"OK");
+    }
   }
 
-  mlen = 0;
-  while(subtitle[mlen]) mlen++;
-  if(mlen>0){
-    SSD1306.ssd1306_setpos(x_mid-mlen*3, y_mid+1);
-    SSD1306.ssd1306_string_font6x8(subtitle);
-  }
+  digitalWrite(LED_PIN, LOW); // end scan
 }
