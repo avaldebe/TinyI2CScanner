29d1f6de971798748f5682bc223b3aff287db948
Alvaro Valdebenito <alvarov@met.no>
2017-09-05 21:33:27 +0200

    simpler layout

--- src/i2c-scanner.ino
+++ src/i2c-scanner.ino
@@ -1,115 +1,44 @@
-// 128x64 OLED module, bitbanged I2C
-#define OLED_MAX_X (128)
-#define OLED_MAX_Y ( 64)
+#include <ssd1306xled.h>  // OLED library, bitbanged I2C
+#include <TinyWireM.h>    // Scan haedware I2C bus
 
-// OLED library, bitbanged I2C
-#include <font6x8.h>
-#include <font8x16.h>
-#include <ssd1306xled.h>
-#include <ssd1306xled8x16.h>
-
-// Scan haedware I2C bus
-#include <TinyWireM.h>
 
 // Background image from tiny44 project
-#include "img.h"
+const uint8_t img8x8[2][8] PROGMEM ={        // squares
+  {0x00,0x7E,0x42,0x42,0x42,0x42,0x7E,0x00}, // empty
+  {0x00,0x7E,0x7E,0x7E,0x7E,0x7E,0x7E,0x00}  // full
+};
 
 void setup() {
-  // init hardware I2C buss
-  TinyWireM.begin();
+#ifdef LED_PIN
+  pinMode(LED_PIN, OUTPUT);         // blink
+#endif
 
-  // blink
-  pinMode(LED_PIN, OUTPUT);
+  TinyWireM.begin();                // init hardware I2C buss
 
-  // init OLED, bitbanged I2C bus
-  SSD1306.ssd1306_init();
+  SSD1306.ssd1306_init();           // init OLED, bitbanged I2C bus
+  SSD1306.ssd1306_fillscreen(0x00); // clear screen
 
-  // welcome message
-  const byte x_mid = OLED_MAX_X/2;
-  const byte y_mid = OLED_MAX_Y/(8*2);
-
-  SSD1306.ssd1306_fillscreen(0x00);
-  SSD1306.ssd1306_char_f8x16(x_mid-12*(8/2), y_mid-1,"I2C Scanner");
-  SSD1306.ssd1306_setpos(x_mid-14*(6/2), y_mid+1);
-  SSD1306.ssd1306_string_font6x8((char*)"7bit addresses");
 }
 
 void blink(uint16_t msec){
+#ifdef LED_PIN
   digitalWrite(LED_PIN, HIGH);
   delay(msec);
   digitalWrite(LED_PIN, LOW);
+#endif
 }  
 
 void loop() {
+  uint8_t addr, col, row;
+  bool found;
+  for(addr=8; addr<120; addr++){    // valid address space
+    TinyWireM.beginTransmission(addr);
+    found = (TinyWireM.endTransmission(1)==0x00);
+    col=addr%16*8;
+    row=addr/16;
+    SSD1306.ssd1306_draw_bmp(col, row, col+8, row+1, img8x8[found?1:0]);
+    if(found) blink(100);
+  }
   delay(2000);
-
-  bool found[128] = {};
-  uint8_t addr, error;
-  for(addr=0; addr<128; addr++){
-    switch(addr){
-    case 8 ... 119:
-      TinyWireM.beginTransmission(addr);
-      error = TinyWireM.endTransmission(1);
-      found[addr] = (error == 0x00);
-      if(found[addr]) blink(5000);
-      break;
-    default:
-      // mark invalid addresses found, only for debugging
-      found[addr] = true;
-    }
-  }
-  
-  SSD1306.ssd1306_setpos(0, 0);
-  uint8_t da=0, nx=17, fill=0, fillcn=4;
-  SSD1306.ssd1306_send_data_start();
-  for(uint16_t y=0; y<8; y++){
-    nx = 17;
-    for(uint16_t x=0; x<128; x++){
-      da = pgm_read_byte(&frame[x+(y*128)]);
-      if((x == nx) && (y > 0)){
-        addr = (((nx-17) / 7)<<3)+y;
-        nx = nx+7;
-        fillcn = 4;
-        switch(y){
-        case 1 :
-          if(found[addr-1]) fill = 0b00011110;
-          break;
-        case 2 :
-          if(found[addr-1]) fill = 0b00001111;
-          if(found[addr])   fill = 0b10000000;
-          break;
-        case 3 :
-          if(found[addr-1]) fill = 0b00000111;
-          if(found[addr])   fill = 0b11000000;
-          break;
-        case 4 :
-          if(found[addr-1]) fill = 0b00000011;
-          if(found[addr])   fill = 0b11100000;
-          break;
-        case 5 :
-          if(found[addr-1]) fill = 0b00000001;
-          if(found[addr])   fill = 0b11110000;
-          break;
-        case 6 :
-          if(found[addr])   fill = 0b01111000;
-          break;
-        case 7 :
-          if(found[addr])   fill = 0b00111100;
-          break;
-        default:
-          fillcn = 0;
-        }
-      }
-      if(fillcn){
-        da |= fill;
-        fillcn--;
-      }else{
-        fill = 0;
-      }
-      SSD1306.ssd1306_send_byte(da);
-    }
-  }
-  SSD1306.ssd1306_send_data_stop();
 }
-
 
