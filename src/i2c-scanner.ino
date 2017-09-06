b9a8383ca1fcc566b65fcc0c7e0346badb01e92b
Alvaro Valdebenito <alvarov@met.no>
2017-09-06 17:59:54 +0200

    clean up unnecessary code

--- src/i2c-scanner.ino
+++ src/i2c-scanner.ino
@@ -1,12 +1,5 @@
-// 128x64 OLED module, bitbanged I2C
-#define OLED_MAX_X (128)
-#define OLED_MAX_Y ( 64)
-
 // OLED library, bitbanged I2C
-#include <font6x8.h>
-#include <font8x16.h>
 #include <ssd1306xled.h>
-#include <ssd1306xled8x16.h>
 
 // Scan haedware I2C bus
 #include <TinyWireM.h>
@@ -18,45 +11,22 @@ void setup() {
   // init hardware I2C buss
   TinyWireM.begin();
 
-  // blink
-  pinMode(LED_PIN, OUTPUT);
-
   // init OLED, bitbanged I2C bus
   SSD1306.ssd1306_init();
 
-  // welcome message
-  const byte x_mid = OLED_MAX_X/2;
-  const byte y_mid = OLED_MAX_Y/(8*2);
-
+  // clean screen
   SSD1306.ssd1306_fillscreen(0x00);
-  SSD1306.ssd1306_char_f8x16(x_mid-12*(8/2), y_mid-1,"I2C Scanner");
-  SSD1306.ssd1306_setpos(x_mid-14*(6/2), y_mid+1);
-  SSD1306.ssd1306_string_font6x8((char*)"7bit addresses");
 }
-
-void blink(uint16_t msec){
-  digitalWrite(LED_PIN, HIGH);
-  delay(msec);
-  digitalWrite(LED_PIN, LOW);
-}  
 
 void loop() {
   delay(2000);
 
   bool found[128] = {};
   uint8_t addr, error;
-  for(addr=0; addr<128; addr++){
-    switch(addr){
-    case 8 ... 119:
-      TinyWireM.beginTransmission(addr);
-      error = TinyWireM.endTransmission(1);
-      found[addr] = (error == 0x00);
-      if(found[addr]) blink(5000);
-      break;
-    default:
-      // mark invalid addresses found, only for debugging
-      found[addr] = true;
-    }
+  for(addr=8; addr<120; addr++){ // valid address space
+    TinyWireM.beginTransmission(addr);
+    error = TinyWireM.endTransmission(1);
+    found[addr] = (error == 0x00);
   }
   
   SSD1306.ssd1306_setpos(0, 0);
