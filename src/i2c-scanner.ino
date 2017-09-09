b3ac4650da8099694d557274bdc3d964300474b2
Alvaro Valdebenito <alvarov@met.no>
2017-09-09 22:02:27 +0200

    tact switch 7b(all)/8b(even) mode

--- src/i2c-scanner.ino
+++ src/i2c-scanner.ino
@@ -1,5 +1,6 @@
 #include <ssd1306xled.h>  // OLED library, bitbanged I2C
 #include <TinyWireM.h>    // Scan haedware I2C bus
+#define TACT_PIN LED_PIN  // same pin for TACT and LED
 
 
 // Background image from tiny44 project
@@ -9,6 +10,7 @@ const uint8_t img8x8[2][8] PROGMEM ={        // squares
 };
 
 void setup() {
+  pinMode(TACT_PIN, INPUT);         // init TACT switch
   TinyWireM.begin();                // init hardware I2C buss
   SSD1306.ssd1306_init();           // init OLED, bitbanged I2C bus
   SSD1306.ssd1306_fillscreen(0x00); // clear screen
@@ -17,25 +19,39 @@ void setup() {
 bool i2c_found(uint8_t addr, uint8_t ntry=1, uint16_t msec=0){
   const uint8_t noError = 0x00;
   uint8_t n = 0;
-  bool found = false;
-  while(ntry>n++ && not found){
+  bool found;
+  do { // test at least once
     TinyWireM.beginTransmission(addr);
     found = (TinyWireM.endTransmission(1) == noError);
     if(msec>0) delay(msec);
-  }
+  } while(ntry>n++ && not found);
   return found;
 }
 
-
 void loop() {
+  static bool colunmFirst = false;
   uint8_t addr, col, row;
   bool found;
-  for(addr=8; addr<120; addr++){    // valid address space
-    col = addr%16*8;
-    row = addr/16;
-    found = i2c_found(addr, 2, 5);
-    SSD1306.ssd1306_draw_bmp(col, row, col+8, row+1, img8x8[found?1:0]);
+  if(colunmFirst){
+     for(addr=8; addr<120; addr++){    // valid address space
+      col = (addr&0x0F)<<3; // equiv (addr%16)*8
+      row = addr>>4;        // equiv  addr/16
+      found = i2c_found(addr, 1, 5);
+      SSD1306.ssd1306_draw_bmp(col, row, col+8, row+1, img8x8[found?1:0]);
+    }
+  }else{
+     for(addr=8; addr<120; addr++){    // valid address space
+      col = addr&0xF8;  // equiv (addr/8)*8
+      row = addr&0x07;  // equiv  addr%8
+      found = i2c_found(addr, 1, 5);
+      SSD1306.ssd1306_draw_bmp(col, row, col+8, row+1, img8x8[found?1:0]);
+    }
   }
-  delay(2000);
+  if(digitalRead(TACT_PIN) == HIGH){
+    colunmFirst = !colunmFirst;
+    SSD1306.ssd1306_fillscreen(0x00); // clear screen
+  }else{
+    delay(2000);
+  }
 }
 
