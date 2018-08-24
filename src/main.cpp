a4670e7f183bcbce083a0127b4a55c7405995044
Alvaro Valdebenito <avaldebe@gmail.com>
2018-08-24 22:11:15 +0200

    reimplement 8bit mode

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ U8G2_SSD1306_128X64_NONAME_1_SW_I2C
 
 #define FONT_TEXT     u8g2_font_5x7_mr
 #define FONT_ICON     u8g2_font_m2icon_7_tf
-#define COL(col)      7*(col)
-#define ROW(row)      7*(row+1)
+#define COL(col)      (7*(col))
+#define ROW(row)      (7*(row+1))
 
 #include <TinyWireM.h>    // Scan haedware I2C bus
 #define TACT_PIN LED_PIN  // same pin for TACT and LED
@@ -17,22 +17,6 @@ void setup() {
   TinyWireM.begin();                // init hardware I2C buss
   oled.begin();                     // init OLED, bitbanged I2C bus
   oled.clear();                     // clear screen
-
-  // header/index
-  oled.setFont(FONT_TEXT);
-  const char header[] = "0123456789ABCDEF";
-  uint8_t n;
-  // print header (1st row)
-  for (n = 0; n<16; n++){
-    oled.drawGlyph(COL(n+1), ROW(0), header[n]);
-  }
-  // print index (1st column)
-  for (n = 0; n<8; n++){
-    oled.drawGlyph(COL(0), ROW(n+1), header[n]);
-  }
-  
-  // fot for icon flyphs
-  oled.setFont(FONT_ICON);
 }
 
 bool i2c_found(uint8_t addr, uint8_t ntry=1, uint16_t msec=0){
@@ -48,16 +32,40 @@ bool i2c_found(uint8_t addr, uint8_t ntry=1, uint16_t msec=0){
 }
 
 void loop() {
-  uint8_t addr;
+  const char header[] = "0123456789ABCDEF";
+  static bool colunmFirst = true;
+  uint8_t addr, n;
   bool found[128];  // firtst and last 8 addresses are reserved
+
   for (addr=8; addr<120; addr++) {  // valid address space
     found[addr] = i2c_found(addr, 2, 5);     // try 2 times for DHT12/AM2320/AM2321
   }
+
   oled.firstPage();
   do {
+    // header/index
+    oled.setFont(FONT_TEXT);
+    // print header (1st row)
+    for (n = 0; n<16; n++){
+      oled.drawGlyph(COL(n+1), ROW(0), header[n]);
+    }
+    // print index (1st column)
+    for (n = 0; n<8; n++){
+      oled.drawGlyph(COL(0), ROW(n+1), colunmFirst?header[n]:header[n*2]);
+    }
+    oled.setFont(FONT_ICON);
     for (addr=8; addr<120; addr++) {  // valid address space
-      oled.drawGlyph(COL(addr%16+1), ROW(addr/16+1), found[addr]?'*':'+');
+      oled.drawGlyph(
+        colunmFirst?COL(addr%16+1):COL(addr/8+1),
+        colunmFirst?ROW(addr/16+1):ROW(addr%8+1),
+        found[addr]?'*':'+'
+      );
     }
   } while ( oled.nextPage() );
-  delay(2000);
+  if (digitalRead(TACT_PIN) == HIGH) {
+    colunmFirst = !colunmFirst;
+    oled.clear();                     // clear screen
+  } else {
+    delay(2000);
+  }
 }
