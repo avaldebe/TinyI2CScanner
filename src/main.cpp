ea7d0bd45471a5942dd7885f1789a0f80ffe296f
Alvaro Valdebenito <avaldebe@gmail.com>
2018-08-25 23:51:26 +0200

    replace header array by HEX1 macro

--- src/main.cpp
+++ src/main.cpp
@@ -31,16 +31,13 @@ bool i2c_found(uint8_t addr, uint8_t ntry=1, uint16_t msec=0){
 
 #define FONT_TEXT       u8g2_font_5x7_mr
 #define FONT_ICON       u8g2_font_m2icon_7_tf
-#define COL(c,a)        (c)?(a%16):(a/8)
-#define ROW(c,a)        (c)?(a/16):(a%8)
-#define HEAD(c,x,y)     header[c?x+y:x+2*y] // use FONT_TEXT
-#define GLYPH(f)        f?0x46:0x45         // use FONT_ICON
-#define XPOS(n)         (n+0)*7
-#define YPOS(n)         (n+1)*7
-const uint8_t header[16] = {
-  '0','1','2','3','4','5','6','7',
-  '8','9','A','B','C','D','E','F'
-};
+#define HEX1(n)         ((n>9)?(n-10+'A'):(n+'0'))  // 0 .. 15 --> '0' .. 'F'
+#define TEXT(c,x,y)     c?HEX1(x+y):HEX1(x+2*y)     // use FONT_TEXT
+#define ICON(f)         f?0x46:0x45                 // use FONT_ICON
+#define COL(c,a)        (c)?(a%16):(a/8)            // low nibble  7b addr || high nibble 8b addr/2
+#define ROW(c,a)        (c)?(a/16):(a%8)            // high nibble 7b addr || low nibble  8b addr/2
+#define XPOS(n)         (n+0)*7                     // text/icon col --> x coord
+#define YPOS(n)         (n+1)*7                     // text/icon row --> y coord
 void loop() {
   static bool colunmFirst = true;
   const uint8_t AM2321 = 0x5c;
@@ -50,17 +47,17 @@ void loop() {
   do {
     oled.setFont(FONT_TEXT);
     for (x=0, y=0; x<16; x++) {       // row0: header
-      oled.drawGlyph(XPOS(x+1), YPOS(y), HEAD(colunmFirst,x,y));
+      oled.drawGlyph(XPOS(x+1), YPOS(y), TEXT(colunmFirst,x,y));
     }
     for (x=0, y=0; y<8; y++) {        // col0: index
-      oled.drawGlyph(XPOS(x), YPOS(y+1), HEAD(colunmFirst,x,y));
+      oled.drawGlyph(XPOS(x), YPOS(y+1), TEXT(colunmFirst,x,y));
     }
     oled.setFont(FONT_ICON);
     for (addr=0; addr<128; addr++) {  // full address spase
        x = COL(colunmFirst, addr);
        y = ROW(colunmFirst, addr);
        found = (addr==AM2321)?i2c_found(addr, 2, 5):i2c_found(addr); // try 2 times for DHT12/AM2320/AM2321
-       oled.drawGlyph(XPOS(x+1), YPOS(y+1), GLYPH(found));
+       oled.drawGlyph(XPOS(x+1), YPOS(y+1), ICON(found));
     }
   } while ( oled.nextPage() );
   if (digitalRead(TACT_PIN) == HIGH) {
