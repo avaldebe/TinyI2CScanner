e6bcf8146ba1b44b984883be5e47e9353abab8cb
Alvaro Valdebenito <avaldebe@gmail.com>
2018-08-25 18:29:31 +0200

    rename u8x8 object

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 
 #include <U8x8lib.h>  // Arduino Monochrome Graphics Library
 U8X8_SSD1306_128X64_NONAME_SW_I2C
-  u8x8(SSD1306_SCL, SSD1306_SDA, U8X8_PIN_NONE); // software I2C
+  oled(SSD1306_SCL, SSD1306_SDA, U8X8_PIN_NONE); // software I2C
 
 void setup() {
-  u8x8.begin();                     // init OLED, bitbanged I2C bus
-  u8x8.clear();                     // clear screen
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.println("OK");
+  oled.begin();                     // init OLED, bitbanged I2C bus
+  oled.clear();                     // clear screen
+  oled.setFont(u8x8_font_chroma48medium8_r);
+  oled.println("OK");
 }
 
 void loop() {
