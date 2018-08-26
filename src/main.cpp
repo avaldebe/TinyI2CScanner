84a1ceb35a46daf1fafd8f2ca5f780173047716b
Alvaro Valdebenito <avaldebe@gmail.com>
2018-08-26 21:45:45 +0200

    interlace u8x8/u2g2 code

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,10 @@
 #ifdef USE_U8X8
   #include <U8x8lib.h>
   #define FONT_TEXT  u8x8_font_chroma48medium8_u
+  #define PAGE_BEGIN
+  #define PAGE_END
+  #define XPOS(n)    n                  // text/icon col --> x coord
+  #define YPOS(n)    n                  // text/icon row --> y coord
   #if defined(SSD1306_SCL) && defined(SSD1306_SDA)
     U8X8_SSD1306_128X64_NONAME_SW_I2C   // software I2C
       oled(SSD1306_SCL, SSD1306_SDA, U8X8_PIN_NONE);
@@ -24,6 +28,10 @@
   #include <U8g2lib.h>
   #define FONT_TEXT  u8g2_font_5x7_mr
   #define FONT_ICON  u8g2_font_m2icon_7_tf
+  #define PAGE_BEGIN oled.firstPage(); do {
+  #define PAGE_END   } while ( oled.nextPage() );
+  #define XPOS(n)    (n+0)*7            // text/icon col --> x coord
+  #define YPOS(n)    (n+1)*7            // text/icon row --> y coord
   #if defined(SSD1306_SCL) && defined(SSD1306_SDA)
     U8G2_SSD1306_128X64_NONAME_1_SW_I2C // software I2C
       oled(U8G2_R0, SSD1306_SCL, SSD1306_SDA, U8X8_PIN_NONE);
@@ -40,17 +48,11 @@ void setup() {
   _Wire.begin();                     // init hardware I2C buss
   oled.begin();                     // init OLED, bitbanged I2C bus
   oled.clear();                     // clear screen
-#ifdef USE_U8X8
-  oled.setFont(FONT_TEXT);
-  oled.println(F("TINY I2C SCANNER"));
-#else
-  oled.firstPage();
-  do {
+  PAGE_BEGIN
     oled.setFont(FONT_TEXT);
-    oled.setCursor(0,7);
-    oled.println(F("Tiny I2C Scanner"));
-  } while ( oled.nextPage() );
-#endif
+    oled.setCursor(XPOS(0),YPOS(4));
+    oled.print(F("TINY I2C SCANNER"));
+  PAGE_END
   delay(1000);
 }
 
@@ -77,8 +79,6 @@ bool scann(uint8_t addr){
 #define ICON(f)         f?0x46:0x45                 // use FONT_ICON
 #define COL(c,a)        (c)?(a%16):(a/8)            // low nibble  7b addr || high nibble 8b addr/2
 #define ROW(c,a)        (c)?(a/16):(a%8)            // high nibble 7b addr || low nibble  8b addr/2
-#define XPOS(n)         (n+0)*7                     // text/icon col --> x coord
-#define YPOS(n)         (n+1)*7                     // text/icon row --> y coord
 void loop() {
 #ifdef TACT_PIN
   static bool colunmFirst = true;
@@ -91,16 +91,8 @@ void loop() {
 #endif
   uint8_t addr, x, y;
   bool found;
-#ifdef USE_U8X8
-  for (addr=0; addr<128; addr++) {  // full address spase
-    x = COL(colunmFirst, addr);
-    y = ROW(colunmFirst, addr);
-    found = scann(addr);
-    oled.drawGlyph(x, y, GLYPH(found,colunmFirst,x,y));
-  }
-#else
-  oled.firstPage();
-  do {
+  PAGE_BEGIN
+#ifndef USE_U8X8
     oled.setFont(FONT_TEXT);
     for (x=0, y=0; x<16; x++) {       // row0: header
       oled.drawGlyph(XPOS(x+1), YPOS(y), TEXT(colunmFirst,x,y));
@@ -109,12 +101,16 @@ void loop() {
       oled.drawGlyph(XPOS(x), YPOS(y+1), TEXT(colunmFirst,x,y));
     }
     oled.setFont(FONT_ICON);
+#endif
     for (addr=0; addr<128; addr++) {  // full address spase
       x = COL(colunmFirst, addr);
       y = ROW(colunmFirst, addr);
       found = scann(addr);
+#ifdef USE_U8X8
+      oled.drawGlyph(x, y, GLYPH(found,colunmFirst,x,y));
+#else
       oled.drawGlyph(XPOS(x+1), YPOS(y+1), ICON(found));
-    }
-  } while ( oled.nextPage() );
 #endif
+    }
+  PAGE_END
 }
